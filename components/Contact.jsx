d352119349110cd982b55cc093408d6c82353a6c
cynthia <santoro.cynthia@yahoo.com>
2023-05-08 20:48:13 -0700

    email

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -70,7 +70,7 @@ const Contact = () => {
           <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
             <div className='p-4'>
               <form
-                action= "mailto:santoro.cynthia@yahoo.com"
+                action= "mailto:kitchencabdesign@aol.com"
                 method='POST'
                 encType='multipart/form-data'
               >
